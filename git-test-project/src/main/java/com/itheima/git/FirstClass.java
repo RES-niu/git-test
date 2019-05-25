f31b857ab692e4f390115ab35e6fde1cedf1a41e
最爱吃虾酱 <2528995057@qq.com>
2019-05-25 11:59:19 +0800

    把本地工程添加到git

---
+++ git-test-project/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,15 @@
+package com.itheima.git;
+
+/**
+ * @author Jeck牛
+ * @create 2019-05-25 11:48
+ **/
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+        System.out.println("hello git project!");
+    }
+}
